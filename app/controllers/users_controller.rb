3a2f225f167b8526f45481d675b603fe8a75f1f4
MarshallSchulte <marshall.schulte@gmail.com>
2014-02-06 17:28:07 -0500

    Sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ def show
   def create
     @user = User.new(user_params)
     if @user.save
+	  sign_in @user
+	  flash[:success] = 'Welcome!'
       redirect_to @user
     else
       render 'new'
